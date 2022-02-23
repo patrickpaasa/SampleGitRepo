3ba99faeaad73147a603323367054f76639097a3
Pating ng AdDU <patrick.paasa@gmail.com>
2022-02-23 15:05:49 +0800

    added firebase-admin; included firebase in app.js; display items in about.ejs

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+const fs = require('firebase-admin');
+
+const serviceAccount = require('./itelective2-firebase-key.json');
+
+fs.initializeApp({
+    credential: fs.credential.cert(serviceAccount)
+});
+
+const db = fs.firestore();
+const itemColl = db.collection('items');
+
+
 app.get('/', function (req, res) {
     let data = {
         url: req.url,
@@ -11,9 +23,14 @@ app.get('/', function (req, res) {
     res.render('pages/index', data);
 });
 
-app.get('/about', function (req, res) {
+app.get('/about', async function (req, res) {
+    const items = await itemColl.get();
+
+
+
     let data = {
         url: req.url,
+        itemData: items.docs,
         array: [
             { fname: "Dodong", lname: "Laboriki" }
         ],
